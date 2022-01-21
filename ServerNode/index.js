7ec136dda3b9c5c75642e1d75216823966bc91b0
Carlos Guagrilla <carlos.guagrilla@hotmail.com>
2022-01-21 17:38:02 -0500

    subida

--- ServerNode/index.js
+++ ServerNode/index.js
@@ -1,6 +1,6 @@
 
-const http = require('http');
-/*const color = require('colors');*/
+/*const http = require('http');
+const colors = require('colors');
 const funcionServidor = function(req, res){
         //res.write("El servidor esta levantado");
         res.write('<h1> Hello world </h1>');
@@ -8,4 +8,43 @@ const funcionServidor = function(req, res){
     }
 const server = http.createServer(funcionServidor);
 server.listen(3000, ()=>{console.log('Servidor levantado')});
+*/
 
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+
+server.get('/', function(req,res){
+    
+    res.json({
+        nombre: "Carlos",
+        apellido: "Guagrilla",
+        edad: 20
+    })
+})
+server.post('/', function(req,res){
+    res.send("<h1>POST raiz</h1>");
+})
+server.put('/', function(req,res){
+    res.send("<h1>PUT raiz</h1>");
+})
+server.delete('/', function(req,res){
+    res.send("<h1>DELETE raiz</h1>");
+})
+//
+server.post('/ingresarUsuario', function(req,res){
+    console.log(req.body);
+    res.send("<h1>POST ingreso de Usuario</h1>");
+})
+server.put('/actualizarUsuario', function(req,res){
+    res.send("<h1>PUT actualizar usuario </h1>");
+})
+server.delete('/eliminarUsuario', function(req,res){
+    res.send("<h1>DELETE eliminar usuario</h1>");
+})
+
+server.listen(3000, function(){
+    console.log("server en el puerto 300");
+})
