d58d56e31a0e40e8680d018bf5e17389edee4816
Carlos Guagrilla <carlos.guagrilla@hotmail.com>
2022-01-26 17:45:50 -0500

    subida

---
+++ ServerNode/ejemploMongo.js
@@ -0,0 +1,45 @@
+/*const objeto = {
+    nombre: "Carlos",
+    apellido: "Guiagrilla",
+    esEstudiante: true,
+    cursa: ["Nuevastecnicas","ProgramacionVisual"],
+    direccion: {
+        callePrincipal: " Mariana de Jesus",
+        calleSecundaria: "San Lorenzo",
+        numero:"s/n"
+    }
+}
+
+const direccion = {
+    callePrincipal: " Mariana de Jesus",
+    calleSecundaria: "San Lorenzo",
+    numero:"s/n"
+}*/
+const director ={
+    nombre: "Jose"
+}
+
+const genero={
+    Nombre:"Accion",
+}
+
+const actor ={
+    nombre:"jhon",
+    sexo:"masculino"
+}
+
+const filmActor ={
+    GanoOscar="si"
+}
+
+const film={
+    titulo:"La momia",
+    oscar: "",
+    año:2021,
+    rating: 12,
+    filmActor: {
+        GanoOscar="si"
+    }
+}
+
+console.log(objeto);
